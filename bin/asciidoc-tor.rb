ad96787434a458ebc7533767e51f01831a20a571
primejade <primejade@proton.me>
2024-10-31 18:30:11 +0330

    merged from PC

--- bin/asciidoc-tor.rb
+++ bin/asciidoc-tor.rb
@@ -5,6 +5,7 @@ require 'asciidoctor/extensions'
 # argument parser
 args = ARGV
 input_file = nil
+theme_file = nil
 output_file = nil
 
 def help
@@ -20,7 +21,7 @@ args.each_with_index do |arg, index|
       help
       exit
     when '-c'
-      theme_file_path = args[index + 1] if index + 1 < args.length
+      theme_file = args[index + 1] if index + 1 < args.length
     when '-i'
       input_file = args[index + 1] if index + 1 < args.length
     when '-o'
@@ -44,17 +45,19 @@ settings = {
   font_code:	'UbuntuMono',
   # font_code:	'JetBrainsMono',
   # font_prepath: Dir.pwd,
-  font_prepath: "/home/hos/.local/share",
+  font_prepath: "/usr/share/fonts/truetype",
+  font_prepath2: "/home/hossein/.local/share/fonts",
   font_size_main: 11.5,
   font_size_code: 10,
   font_size_inlinecode: 10,
   font_size_caption: 12.5,
   number: true,
   # cap_color: "#ff7700",
-  cap_color:	"#7a2518",
-  code_color:	"#25187a",
+  # cap_color:	"#7a2518",
+  # code_color:	"#25187a",
   doc_author: "Hos Es",
-  doc_syntax: "bw", # pygments: algol, friendly_grayscale; rouge: base16.solarized.light
+  doc_highlight: "pygments", # pygments, rouge
+  doc_syntax: "solarized-dark", # pygments: algol, friendly_grayscale, bw; rouge: base16.solarized.light
   # doc_lang: "lua",
   doc_name: input_file,
   h_dlist: false,
@@ -74,88 +77,211 @@ if settings[:h_dlist]
   end
 end
 
-# fonts settings
+# theme  
 
-# list of fonts
+# colors {{{
+colorscheme = {
+
+  'github' => {
+    background:   "#0d1117",
+    text:         "#f0f6fc",
+    title:        '#9198a1',
+    link:         '#4493f8',
+    dim:          "#232323",
+    border:       "#9198a1",
+    table_border: "#2f353d",
+    table_bg_1:   "#151b23",
+    table_bg_2:   "#0d1117",
+    quote_text:   '#9198a1',
+    quote_border: '#3d444d',
+    sidebar:      '#151b23',
+    code_bg:      '#1e242a',
+    code_border:  '#3d444d',
+    highlight:    '#f78166',
+    note:         '#6a9e03',
+    tip:          '#ffffff',
+    caution:      '#9e0303',
+    wanring:      '#9e6a03',
+    important:    '#8957e5',
+  },
+
+  'dracula' => {
+    background:   "#1e1f28",
+    text:         "#f8f8f2",
+    title:        '#bbbbbb',
+    link:         '#8ae9fc',
+    dim:          "#21262d",
+    border:       "#8b949e",
+    table_border: "#484f58",
+    table_bg_1:   "#21262d",
+    table_bg_2:   "#161b22",
+    quote_text:   '#6e7681',
+    quote_border: '#323543',
+    sidebar:      '#000000',
+    code_bg:      '#1e1f28',
+    code_border:  '#545454',
+    highlight:    '#f78166',
+    note:         '#50fa7b',
+    tip:          '#ffffff',
+    caution:      '#ff5454',
+    wanring:      '#f0fa8b',
+    important:    '#bd92f8',
+  },
+
+  'solarized.dark' => {
+    background:   "#001e26",
+    text:         "#fdf6e3",
+    title:        '#839496',
+    link:         '#c61b6e',
+    dim:          "#25292a",
+    border:       "#839496",
+    table_border: "#484f58",
+    table_bg_1:   "#002731",
+    table_bg_2:   "#073642",
+    quote_text:   '#6e7681',
+    quote_border: '#484f58',
+    sidebar:      '#073642',
+    code_bg:      '#002b36',
+    code_border:  '#484f58',
+    highlight:    '#2075c7',
+    note:         '#728905',
+    tip:          '#cccccc',
+    caution:      '#d01b24',
+    wanring:      '#a57705',
+    important:    '#6c71c4',
+  },
+
+  'gruvbox.dark' => {
+    background:   '#',
+    text:         '#',
+    title:        '#',
+    link:         '#',
+    dim:          '#',
+    border:       '#',
+    table_border: '#',
+    table_bg_1:   '#',
+    table_bg_2:   '#',
+    quote_text:   '#',
+    quote_border: '#',
+    sidebar:      '#',
+    code_bg:      '#',
+    code_border:  '#',
+    highlight:    '#',
+    note:         '#',
+    tip:          '#',
+    caution:      '#',
+    wanring:      '#',
+    important:    '#',
+  },
+
+}
+# }}}
+
+body_theme = colorscheme['solarized.dark']
+
+# list of fonts {{{
 font_catalog = {
     'FreeSerif' => {
-        normal:      '/home/hos/.local/share/fonts/gnu-freefonts/serif/FreeSerif.ttf',
-        bold:        '/home/hos/.local/share/fonts/gnu-freefonts/serif/FreeSerifBold.ttf',
-        italic:      '/home/hos/.local/share/fonts/gnu-freefonts/serif/FreeSerifItalic.ttf',
-        bold_italic: '/home/hos/.local/share/fonts/gnu-freefonts/serif/FreeSerifBoldItalic.ttf',
+        normal:      '/home/hossein/.local/share/fonts/gnu-freefonts/serif/FreeSerif.ttf',
+        bold:        '/home/hossein/.local/share/fonts/gnu-freefonts/serif/FreeSerifBold.ttf',
+        italic:      '/home/hossein/.local/share/fonts/gnu-freefonts/serif/FreeSerifItalic.ttf',
+        bold_italic: '/home/hossein/.local/share/fonts/gnu-freefonts/serif/FreeSerifBoldItalic.ttf',
     },
     'FreeSans' => {
-        normal:      '/home/hos/.local/share/fonts/gnu-freefonts/sans/FreeSans.ttf',
-        bold:        '/home/hos/.local/share/fonts/gnu-freefonts/sans/FreeSansBold.ttf',
-        italic:      '/home/hos/.local/share/fonts/gnu-freefonts/sans/FreeSansOblique.ttf',
-        bold_italic: '/home/hos/.local/share/fonts/gnu-freefonts/sans/FreeSansBoldOblique.ttf',
+        normal:      '/home/hossein/.local/share/fonts/gnu-freefonts/sans/FreeSans.ttf',
+        bold:        '/home/hossein/.local/share/fonts/gnu-freefonts/sans/FreeSansBold.ttf',
+        italic:      '/home/hossein/.local/share/fonts/gnu-freefonts/sans/FreeSansOblique.ttf',
+        bold_italic: '/home/hossein/.local/share/fonts/gnu-freefonts/sans/FreeSansBoldOblique.ttf',
     },
     'FreeMono' => {
-        normal:      '/home/hos/.local/share/fonts/gnu-freefonts/mono/FreeMono.ttf',
-        bold:        '/home/hos/.local/share/fonts/gnu-freefonts/mono/FreeMonoBold.ttf',
-        italic:      '/home/hos/.local/share/fonts/gnu-freefonts/mono/FreeMonoOblique.ttf',
-        bold_italic: '/home/hos/.local/share/fonts/gnu-freefonts/mono/FreeMonoBoldOblique.ttf',
+        normal:      '/home/hossein/.local/share/fonts/gnu-freefonts/mono/FreeMono.ttf',
+        bold:        '/home/hossein/.local/share/fonts/gnu-freefonts/mono/FreeMonoBold.ttf',
+        italic:      '/home/hossein/.local/share/fonts/gnu-freefonts/mono/FreeMonoOblique.ttf',
+        bold_italic: '/home/hossein/.local/share/fonts/gnu-freefonts/mono/FreeMonoBoldOblique.ttf',
     },
     'LiberationSerif' => {
-        normal:      settings[:font_prepath] + '/fonts/liberation/LiberationSerif-Regular.ttf',
-        bold:        settings[:font_prepath] + '/fonts/liberation/LiberationSerif-Bold.ttf',
-        italic:      settings[:font_prepath] + '/fonts/liberation/LiberationSerif-Italic.ttf',
-        bold_italic: settings[:font_prepath] + '/fonts/liberation/LiberationSerif-BoldItalic.ttf',
+        normal:      settings[:font_prepath] + '/liberation/LiberationSerif-Regular.ttf',
+        bold:        settings[:font_prepath] + '/liberation/LiberationSerif-Bold.ttf',
+        italic:      settings[:font_prepath] + '/liberation/LiberationSerif-Italic.ttf',
+        bold_italic: settings[:font_prepath] + '/liberation/LiberationSerif-BoldItalic.ttf',
     },
     'LiberationSans' => {
-        normal:      settings[:font_prepath] + '/fonts/liberation/LiberationSans-Regular.ttf',
-        bold:        settings[:font_prepath] + '/fonts/liberation/LiberationSans-Bold.ttf',
-        italic:      settings[:font_prepath] + '/fonts/liberation/LiberationSans-Italic.ttf',
-        bold_italic: settings[:font_prepath] + '/fonts/liberation/LiberationSans-BoldItalic.ttf',
+        normal:      settings[:font_prepath] + '/liberation/LiberationSans-Regular.ttf',
+        bold:        settings[:font_prepath] + '/liberation/LiberationSans-Bold.ttf',
+        italic:      settings[:font_prepath] + '/liberation/LiberationSans-Italic.ttf',
+        bold_italic: settings[:font_prepath] + '/liberation/LiberationSans-BoldItalic.ttf',
     },
     'LiberationMono' => {
-        normal:      settings[:font_prepath] + '/fonts/liberation/LiberationMono-Regular.ttf',
-        bold:        settings[:font_prepath] + '/fonts/liberation/LiberationMono-Bold.ttf',
-        italic:      settings[:font_prepath] + '/fonts/liberation/LiberationMono-Italic.ttf',
-        bold_italic: settings[:font_prepath] + '/fonts/liberation/LiberationMono-BoldItalic.ttf',
+        normal:      settings[:font_prepath] + '/liberation/LiberationMono-Regular.ttf',
+        bold:        settings[:font_prepath] + '/liberation/LiberationMono-Bold.ttf',
+        italic:      settings[:font_prepath] + '/liberation/LiberationMono-Italic.ttf',
+        bold_italic: settings[:font_prepath] + '/liberation/LiberationMono-BoldItalic.ttf',
     },
     'JetBrainsMono' => {
-        normal:      settings[:font_prepath] + '/fonts/jetbrains/JetBrainsMono-Regular.ttf',
-        bold:        settings[:font_prepath] + '/fonts/jetbrains/JetBrainsMono-Bold.ttf',
-        italic:      settings[:font_prepath] + '/fonts/jetbrains/JetBrainsMono-Italic.ttf',
-        bold_italic: settings[:font_prepath] + '/fonts/jetbrains/JetBrainsMono-BoldItalic.ttf',
+        normal:      settings[:font_prepath] + '/jetbrains/JetBrainsMono-Regular.ttf',
+        bold:        settings[:font_prepath] + '/jetbrains/JetBrainsMono-Bold.ttf',
+        italic:      settings[:font_prepath] + '/jetbrains/JetBrainsMono-Italic.ttf',
+        bold_italic: settings[:font_prepath] + '/jetbrains/JetBrainsMono-BoldItalic.ttf',
     },
     'UbuntuMono' => {
-        normal:      settings[:font_prepath] + '/fonts/ubuntu/UbuntuMono-R.ttf',
-        bold:        settings[:font_prepath] + '/fonts/ubuntu/UbuntuMono-B.ttf',
-        italic:      settings[:font_prepath] + '/fonts/ubuntu/UbuntuMono-RI.ttf',
-        bold_italic: settings[:font_prepath] + '/fonts/ubuntu/UbuntuMono-BI.ttf',
+        normal:      settings[:font_prepath] + '/ubuntu/UbuntuMono-R.ttf',
+        bold:        settings[:font_prepath] + '/ubuntu/UbuntuMono-B.ttf',
+        italic:      settings[:font_prepath] + '/ubuntu/UbuntuMono-RI.ttf',
+        bold_italic: settings[:font_prepath] + '/ubuntu/UbuntuMono-BI.ttf',
     },
     'Ubuntu' => {
-        bold:        settings[:font_prepath] + '/fonts/ubuntu/Ubuntu-B.ttf',
-        normal:      settings[:font_prepath] + '/fonts/ubuntu/Ubuntu-R.ttf',
-        italic:      settings[:font_prepath] + '/fonts/ubuntu/Ubuntu-RI.ttf',
-        bold_italic: settings[:font_prepath] + '/fonts/ubuntu/Ubuntu-BI.ttf',
+        bold:        settings[:font_prepath] + '/ubuntu/Ubuntu-B.ttf',
+        normal:      settings[:font_prepath] + '/ubuntu/Ubuntu-R.ttf',
+        italic:      settings[:font_prepath] + '/ubuntu/Ubuntu-RI.ttf',
+        bold_italic: settings[:font_prepath] + '/ubuntu/Ubuntu-BI.ttf',
     },
     'CMUSerif' => {
-        normal:      '/usr/share/fonts/truetype/cmu/cmunrm.ttf',
-        bold:        '/usr/share/fonts/truetype/cmu/cmunbx.ttf',
-        italic:      '/usr/share/fonts/truetype/cmu/cmunti.ttf',
-        bold_italic: '/usr/share/fonts/truetype/cmu/cmunbi.ttf',
+        normal:      settings[:font_prepath2] + '/cmu/cmunrm.ttf',
+        bold:        settings[:font_prepath2] + '/cmu/cmunbx.ttf',
+        italic:      settings[:font_prepath2] + '/cmu/cmunti.ttf',
+        bold_italic: settings[:font_prepath2] + '/cmu/cmunbi.ttf',
     },
     'CMUMono' => {
-        normal:      '/usr/share/fonts/truetype/cmu/cmuntt.ttf',
-        bold:        '/usr/share/fonts/truetype/cmu/cmuntb.ttf',
-        italic:      '/usr/share/fonts/truetype/cmu/cmunit.ttf',
-        bold_italic: '/usr/share/fonts/truetype/cmu/cmuntx.ttf',
+        normal:      settings[:font_prepath2] + '/cmu/cmuntt.ttf',
+        bold:        settings[:font_prepath2] + '/cmu/cmuntb.ttf',
+        italic:      settings[:font_prepath2] + '/cmu/cmunit.ttf',
+        bold_italic: settings[:font_prepath2] + '/cmu/cmuntx.ttf',
     },
 }
 
 font_main = settings[:font_main]
 font_title = settings[:font_title]
 font_code = settings[:font_code]
+
 current_font_main = font_catalog[font_main]
 current_font_title = font_catalog[font_title]
 current_font_code = font_catalog[font_code]
+# }}}
 
-# yaml
+# puts '---------------'
+# puts current_font_main
+# puts '---------------'
+# puts current_font_title
+# puts '---------------'
+# puts current_font_code
+# puts '---------------'
+
+# puts '---------------'
+# puts Dir.pwd
+# puts '---------------'
+
+# current_font_main.each { |x| puts x[1] }
+# current_font_title.each { |x| puts x[1] }
+# current_font_code.each { |x| puts x[1] }
+
+# yaml {{{
 theme_content = <<-YAML
 extends: default
+page:
+  background-color: #{body_theme[:background]}
+  size: a4paper
+  # layout: portrait
+  # margin: [0.75in, 1in, 0.75in, 1in]
 font:
   catalog:
     Noto Serif:
@@ -174,24 +300,79 @@ font:
       italic:      #{current_font_title[:italic]}
       bold_italic: #{current_font_title[:bold_italic]}
 base:
+  font-color: #{body_theme[:text]}
+  color: #{body_theme[:background]}
   #font-family: #{settings[:font_main]}
   font-size: #{settings[:font_size_main]}
-code:
-  #font-family: #{settings[:font_code]}
-  font-size: #{settings[:font_size_code]}
-codespan:
-  #font-family: #{settings[:font_code]}
-  font-size: #{settings[:font_size_inlinecode]}
-  font-color: #{settings[:code_color]}
+link:
+  font-color: #{body_theme[:link]}
+  # text-decoration: underline
+  text-decoration-color: #{body_theme[:link]}
 kbd:
   #font-family: #{settings[:font_code]}
 button:
   #font-family: #{settings[:font_main]}
+prose:
+  text-indent-inner: 18
 caption:
-  font-color: #{settings[:cap_color]}
+  font-color: #{body_theme[:title]}
   font-size: #{settings[:font_size_caption]}
   font-style: bold # bold_italic
+code:
+  #font-family: #{settings[:font_code]}
+  font-size: #{settings[:font_size_code]}
+example:
+  background-color: #{body_theme[:dim]}
+  border-radius: 0.5
+  border-width: 0.0
+codespan:
+  #font-family: #{settings[:font_code]}
+  font-size: #{settings[:font_size_inlinecode]}
+  font-color: #{body_theme[:text]}
+  border-color: #{body_theme[:border]}
+  border-offset: 1.5
+  border-radius: 1.5
+  background-color: #{body_theme[:code_bg]}
+code:
+  background-color: #{body_theme[:code_bg]}
+  border-color: #{body_theme[:code_border]}
+  font-color: #{body_theme[:text]}
+sidebar:
+  background-color: #{body_theme[:sidebar]}
+  border-color: #{body_theme[:border]}
+  border-radius: 1.0
+  border-width: 0.4
+  title:
+    font-color: #{body_theme[:text]}
+quote:
+  font-color: #{body_theme[:quote_text]} 
+  border-color: #{body_theme[:quote_border]} 
+  border-width: 0.0
+table:
+  background-color: #{body_theme[:table_bg_2]}
+  border-color: #{body_theme[:table_border]}
+  head:
+    background-color: #{body_theme[:table_bg_2]}
+  body:
+    stripe-background-color: #{body_theme[:table_bg_1]}
+admonition:
+  column-rule-style: dashed
+  border-style: double
+  label:
+    # font-color: #262626
+  icon:
+    note:
+      stroke-color: #{body_theme[:note]}
+    warning:
+      stroke-color: #{body_theme[:wanring]}
+    caution:
+      stroke-color: #{body_theme[:caution]}
+    important:
+      stroke-color: #{body_theme[:important]}
+    tip:
+      stroke-color: #{body_theme[:tip]}
 heading:
+  font-color: #{body_theme[:text]}
   h1-font-family: "Title Sans"
   h2-font-family: "Title Sans"
   h3-font-family: "Title Sans"
@@ -206,14 +387,18 @@ heading:
   h6-font-size: $base_font_size_small
 YAML
 
-if input_file.nil?
-  theme_file_path = 'asciidoctor-theme.yml'
-  File.open(theme_file_path, 'w') do |file|
-    file.write(theme_content)
-  end
+if theme_file.nil?
+  theme_file_path = Dir.pwd+'/asciidoctor-theme.yml'
+else
+  theme_file_path = theme_file
 end
 
-# options
+File.open(theme_file_path, 'w') do |file|
+  file.write(theme_content)
+end
+# }}}
+
+# options 
 options = {
     backend: 'pdf',
     safe: :unsafe,
@@ -224,18 +409,17 @@ options = {
     # standalone: false,
 
     attributes: {
-        "imagesdir" => "images",
+        # "imagesdir" => "images",
         "skip-front-matter" => true,
         "author" => settings[:doc_author],
         "icons" => "font",
         "icon-set" => "fi",
+        "table-stripes" => "even",
         'toc' => false,
         'sectnums' => settings[:number],
         'linenums' => false,
-        'source-highlighter' => 'pygments',
+        'source-highlighter' => settings[:doc_highlight],
         'pygments-style' => settings[:doc_syntax],
-        # 'source-highlighter' => 'rouge',
-        # 'rouge-style' => settings[:doc_syntax],
         'pdf-themedir' => '.',
         'pdf-theme' => theme_file_path,
     }
--- bin/randr.sh
+++ bin/randr.sh
@@ -10,31 +10,45 @@ ResX="1920"
 ResY="1080"
 Res="${ResX}x${ResY}"
 
+# xrandr="$(xrandr | grep connect | grep -v dis | cut -d' ' -f1)"
+#
+# PRIMARY="echo ${xrandr} | grep DP"
+# SECONDARY="echo ${xrandr} | grep SECONDARY"
+#
+# echo $xrandr $PRIMARY $SECONDARY
+# exit 23
+
+PRIMARY="HDMI-2"
+SECONDARY="DP-2"
+
 #if [ ${ResX} -gt 1920 ] || [ ${ResX} -gt 1080 ]; then
 
 ## old setup of dual-mon
 #			--primary --mode ${Res} --pos 0x${ResY} --rotate normal \
-#		--output HDMI-1 \
+#		--output SECONDARY-1 \
 #			--mode ${Res} --pos 0x0 --rotate normal \
-#			--auto --mode ${Res} --right-of eDP-1 --rotate normal \
+#			--auto --mode ${Res} --left-of eDP-1 --rotate normal \
 
-if [ "${1}" -eq 2 ]; then
+if [ "${1}" = '2' ]; then
 	xrandr \
-		--output eDP1 \
+		-r 75 \
+		--output ${PRIMARY} \
 			--primary --auto --mode ${Res} --rotate normal \
-		--output HDMI1 \
-			--auto --mode ${Res} --right-of eDP1 --rotate normal \
-#		--output HDMI-2 --off \
+		--output ${SECONDARY} \
+		-r 75 \
+            --auto --mode ${Res} --left-of ${PRIMARY} --rotate normal \
+#		--output SECONDARY-2 --off \
 #		--output DP-1 --off \
 #		--output DP-1-0 --off \
 #		--output DP-1-1 --off \
 #		--output DP-1-2 --off \
 #		--output DP-1-3 --off
-elif [ "${1}" -eq 0 ]; then
+elif [ "${1}" = '0' ]; then
 	xrandr \
-		--output eDP1 \
-			--auto --same-as HDMI1 --mode ${Res} \
-		--output HDMI1 \
+		-r 75 \
+		--output ${PRIMARY} \
+			--auto --same-as ${SECONDARY} --mode ${Res} \
+		--output ${SECONDARY} \
 			--mode ${Res} \
 		--output HDMI2 --off \
 		--output DP-1 --off \
@@ -44,9 +58,10 @@ elif [ "${1}" -eq 0 ]; then
 		--output DP-1-3 --off
 else
 	xrandr \
-		--output eDP1 \
+		-r 75 \
+		--output ${PRIMARY} \
 			--primary --auto --mode ${Res} --pos 0x${ResY} --rotate normal \
-		--output HDMI1 --off \
+		--output ${SECONDARY} --off \
 		--output DP-1 --off \
 		--output HDMI2 --off \
 		--output DP-1-0 --off \
@@ -58,5 +73,6 @@ fi
 notify-send -u low -a xrandr "resolution is set ${Res}"
 sleep 1
 
-xwallpaper --no-randr --zoom ~/.local/share/dwm/background.jpg
+# xwallpaper --no-randr --zoom ~/.local/share/dwm/background.jpg
+xwallpaper --zoom ~/.local/share/dwm/background.jpg
 notify-send -u low -a xwallpaper "background is set"
